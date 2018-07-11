9e293391767c77df38a0598f0e6f897c86b39383
Alice BOSSARD <abossard@e3r2p20.42.fr>
2018-07-11 07:29:36 +0200

    coucou mimi

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abossard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/04 07:14:05 by abossard          #+#    #+#             */
-/*   Updated: 2018/07/10 06:56:41 by abossard         ###   ########.fr       */
+/*   Updated: 2018/07/11 07:28:28 by abossard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ int		count_args(const char *format, t_infos *p)
 		}
 		I++;
 	}
+	printf("nbr d'arg = %d\n", NBR);
 	return (NBR);
 }
 
@@ -41,7 +42,7 @@ void	fill_list(const char *format, t_infos *p, va_list ap)
 	t_args	*ptr;
 
 	I = 0;
-
+	NBR = count_args(format, p);
 	while (I < NBR)
 	{
 		ptr = init_args(&p->args_beg);
@@ -54,19 +55,38 @@ int		ft_printf(const char *format, ...)
 {
 	va_list		ap;
 	t_infos		*p;
-	t_args		*q;
+	t_params	*par;
 
-	va_start(ap, format);
 	p = ft_memalloc(sizeof(t_infos));
-	NBR = count_args(format, p);
+	I = 0;
+	J = 0;
+	par = init_params(&p->para_beg);
+	va_start(ap, format);
 	fill_list(format, p, ap);
-	q = p->args_beg;
-	while (q != NULL)
+	while (format[I] != '\0')
 	{
-		printf("fill list = %s\n", q->arg);
-		q = q->next;
+		if (format[I] == '%')
+		{
+			par->str = ft_strsub(format, J, (I - J));
+			J = I;
+			par->size_str = ft_strlen(par->str);
+			par = init_params(&p->para_beg);
+			while(format[I] != 'n' && format[I] != 'c' && format[I]
+					!= 'd' && format[I] != 'i' && format[I] != 'x' &&
+					format[I] != 'X' && format[I] != 'p' && 
+					format[I] != 'e' && format[I] != 'E' &&
+					format[I] != 'F' && format[I] != 'f' &&
+					format[I] != 'g' && format[I] != 'G' &&
+					format[I] != 'o' && format[I] != 's' &&
+					format[I] != '%' && format[I] != '\0')
+				I++;
+			parse_args(ft_strsub(format, J, ((I - J) + 1)), p, par);
+			J = I;
+			par = init_params(&p->para_beg);
+		}
+		I++;
 	}
-	printf("nbr d'arg = %d\n", NBR);
+	par->str = ft_strsub(format, J, (I - J));
 	return (0);
 }
 
